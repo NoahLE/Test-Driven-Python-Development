102003c083335716d0a094dc9f83f58ad4e004a4
NoahLE <noah@noahle.com>
2018-01-16 11:26:33 -0800

    first steps of our ft for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -66,7 +66,6 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith wonders whether the site will remember her list.
         # Then she sees that the site has generated a unique URL for her - there is some explanatory text to that effect
 
-
         # She visits that URL - her to-do list is still there
 
         # Satisfied, she goes back to sleep
@@ -112,3 +111,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
