eb66bc121795406945d65416493982c376864e53
NoahLE <noah@noahle.com>
2018-01-16 10:31:45 -0800

    Done with chapter 7 - https://www.obeythetestinggoat.com/book/chapter_working_incrementally.html

---
+++ lists/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from lists import views
+
+urlpatterns = [
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+]
